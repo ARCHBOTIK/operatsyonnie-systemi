8a5ee940efc932c0915f2b57e9d2acc4123c54d1
mdubkov06 <maksim.dubkov.2006@mail.ru>
2026-03-18 21:21:08 +0400

    Update - change Settings.razor

--- SecurePassword/SecurePassword/MainPage.xaml.cs
+++ SecurePassword/SecurePassword/MainPage.xaml.cs
@@ -1,10 +1,32 @@
-﻿namespace SecurePassword
+﻿using Microsoft.AspNetCore.Components.WebView;
+using Microsoft.Maui.Controls;
+#if WINDOWS
+using Microsoft.UI.Xaml.Controls;
+#endif
+namespace SecurePassword
 {
+
     public partial class MainPage : ContentPage
     {
         public MainPage()
         {
             InitializeComponent();
         }
-    }
+#if WINDOWS
+        private void blazorWebView_BlazorWebViewInitializing(object? sender, BlazorWebViewInitializingEventArgs e)
+        {
+
+        // Пока можно оставить пустым
+
+        }
+
+        private void blazorWebView_BlazorWebViewInitialized(object? sender, BlazorWebViewInitializedEventArgs e)
+        {
+        if (e.WebView is WebView2 webView && webView.CoreWebView2 is not null)
+        {
+            webView.CoreWebView2.Settings.IsZoomControlEnabled = false;
+        }
+        }
+#endif
+}
 }
