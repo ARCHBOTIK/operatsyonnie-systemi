4a4edf58eff72628264622ebc6e3b08d8c9729e2
KogdaBudetPython <puponok@gmail.com>
2026-03-28 20:46:34 +0400

    Internal classes for TCP protocol
    Realisation of TCP protocol for data importation/exoprtation

---
+++ SecurePassword/SecurePassword/sql(rus)/NetworkProtocols/PacketProtocol.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace SecurePassword.SQL_Rus_.NetworkProtocols
+{
+    internal class PacketProtocol
+    {
+        public const int MAX_DATA_LENGTH = 10000000;
+        public static async Task WritePacketAsync(NetworkStream stream, byte[] data, CancellationToken token = default)
+        {
+            if (stream == null) throw new ArgumentNullException(nameof(stream));
+            if (data == null) throw new ArgumentNullException(nameof(data));
+
+            int length = IPAddress.HostToNetworkOrder(data.Length);
+            byte[] lengthBytes = BitConverter.GetBytes(length);
+
+            await stream.WriteAsync(lengthBytes, 0, 4, token);
+            await stream.WriteAsync(data, 0, data.Length, token);
+            await stream.FlushAsync(token);
+        }
+
+        public static async Task<byte[]> ReadPacketAsync(NetworkStream stream, CancellationToken token = default)
+        {
+            if (stream == null) throw new ArgumentNullException(nameof(stream));
+
+            byte[] lengthBytes = await ReadExactAsync(stream, 4, token);
+            int length = IPAddress.NetworkToHostOrder(BitConverter.ToInt32(lengthBytes));
+
+            if (length <= 0 || length > MAX_DATA_LENGTH) throw new Exception("Invalid packet size!");
+
+            return await ReadExactAsync(stream, length, token);
+        }
+
+        public static async Task<byte[]> ReadExactAsync(NetworkStream stream, int size, CancellationToken token = default)
+        {
+            if (stream == null) throw new ArgumentNullException(nameof(stream));
+            if (size < 0) throw new ArgumentOutOfRangeException(nameof(size));
+
+            byte[] buffer = new byte[size];
+            int read = 0;
+
+            while (read < size)
+            {
+                int r = await stream.ReadAsync(buffer, read, size - read, token);
+                if (r == 0) throw new Exception("Disconnected!");
+                read += r;
+            }
+
+            return buffer;
+        }
+    }
+}
---
+++ SecurePassword/SecurePassword/sql(rus)/NetworkProtocols/TcpServer.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+
+namespace SecurePassword.SQL_Rus_.NetworkProtocols
+{
+    internal class TcpServer
+    {
+        private TcpListener _listener;
+        private CancellationTokenSource _cts;
+        private TcpClient _client;
+        public long SentPackets => Interlocked.Read(ref _sentPackets);
+        public long ReceivedPackets => Interlocked.Read(ref _receivedPackets);
+
+        private long _sentPackets;
+        private long _receivedPackets;
+        public event Action<byte[]> DataReceived;
+        public event Action ClientConnected;
+        public event Action ClientDisconnected;
+        private bool _isRunning;
+        public bool IsRunning => _isRunning;
+
+        public Task StartAsync(string ip, int port)
+        {
+            if (_isRunning) return Task.CompletedTask;
+            _cts = new CancellationTokenSource();
+            _listener = new TcpListener(IPAddress.Parse(ip), port);
+            _listener.Start();
+            _isRunning = true;
+            _ = AcceptLoop(_cts.Token);
+            return Task.CompletedTask;
+        }
+
+        public void Stop()
+        {
+            if (!_isRunning) return;
+            _isRunning = false;
+            _cts?.Cancel();
+            try
+            {
+                _listener?.Stop();
+            }
+            catch { }
+            try
+            {
+                _client.Close();
+            }
+            catch { }
+            return;
+        }
+
+        private async Task AcceptLoop(CancellationToken token)
+        {
+            var listener = _listener;
+            try
+            {
+                while (!token.IsCancellationRequested)
+                {
+                    var client = await listener.AcceptTcpClientAsync();
+                    ClientConnected?.Invoke();
+                    _ = ReceiveLoop(client, token);
+                }
+            }
+            catch { }
+
+        }
+
+        private async Task ReceiveLoop(System.Net.Sockets.TcpClient client, CancellationToken token)
+        {
+            var stream = client.GetStream();
+            try
+            {
+                while(!token.IsCancellationRequested)
+                {
+                    byte[] lengthBytes = await PacketProtocol.ReadExactAsync(stream, 4, token);
+                    int length = IPAddress.NetworkToHostOrder(BitConverter.ToInt32(lengthBytes, 0));
+                    if (length <= 0 || length > PacketProtocol.MAX_DATA_LENGTH) throw new Exception("Invalid packet size!");
+                    byte[] data = await PacketProtocol.ReadExactAsync(stream, length, token);
+                    Interlocked.Increment(ref _receivedPackets);
+                    DataReceived?.Invoke(data);
+                }
+            }
+            catch { }
+            client.Close();
+            ClientDisconnected.Invoke();
+        }
+
+        public async Task SendAsync(System.Net.Sockets.TcpClient client, byte[] data)
+        {
+            var stream = client.GetStream();
+            int length = IPAddress.HostToNetworkOrder(data.Length);
+            byte[] lengthBytes = BitConverter.GetBytes(length);
+            await stream.WriteAsync(lengthBytes, 0, 4);
+            await stream.WriteAsync(data, 0, data.Length);
+            Interlocked.Increment(ref _sentPackets);
+        }
+    }
+}
