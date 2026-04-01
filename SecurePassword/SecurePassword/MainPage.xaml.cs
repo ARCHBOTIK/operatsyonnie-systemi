e9b94482dca64463be1d0dd1f4ec2596336fa32f
Hillariot <hillariot2070@gmail.com>
2026-04-01 18:11:55 +0400

    Update MainPage.xaml.cs

--- SecurePassword/SecurePassword/MainPage.xaml.cs
+++ SecurePassword/SecurePassword/MainPage.xaml.cs
@@ -1,32 +1,39 @@
 ﻿using Microsoft.AspNetCore.Components.WebView;
+using Microsoft.AspNetCore.Components.WebView.Maui;
 using Microsoft.Maui.Controls;
+
 #if WINDOWS
 using Microsoft.UI.Xaml.Controls;
 #endif
+
 namespace SecurePassword
 {
-
     public partial class MainPage : ContentPage
     {
         public MainPage()
         {
             InitializeComponent();
         }
+
+        private void blazorWebView_BlazorWebViewInitializing(
+            object sender,
+            BlazorWebViewInitializingEventArgs e)
+        {
 #if WINDOWS
-        private void blazorWebView_BlazorWebViewInitializing(object? sender, BlazorWebViewInitializingEventArgs e)
-        {
-
-        // Пока можно оставить пустым
-
-        }
-
-        private void blazorWebView_BlazorWebViewInitialized(object? sender, BlazorWebViewInitializedEventArgs e)
-        {
-        if (e.WebView is WebView2 webView && webView.CoreWebView2 is not null)
-        {
-            webView.CoreWebView2.Settings.IsZoomControlEnabled = false;
-        }
-        }
+            // Пока можно оставить пустым или добавить Windows-логику
 #endif
-}
+        }
+
+        private void blazorWebView_BlazorWebViewInitialized(
+            object sender,
+            BlazorWebViewInitializedEventArgs e)
+        {
+#if WINDOWS
+            if (e.WebView is WebView2 webView && webView.CoreWebView2 is not null)
+            {
+                webView.CoreWebView2.Settings.IsZoomControlEnabled = false;
+            }
+#endif
+        }
+    }
 }
